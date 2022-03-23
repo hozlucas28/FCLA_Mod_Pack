4ae9a31460d6b8ec5e2df710d4b9ed21f434ce07
hozlucas28 <hozlucas28@gmail.com>
2022-03-23 01:21:50 -0300

    Update

---
+++ README.md
@@ -0,0 +1,8 @@
+**¡FCLA Mod Pack trata de un conjunto de mods (modificados) para la comunidad de Arma 3 FCLA!**
+* Página de la comunidad: https://arma3clanfcla.wixsite.com/fcla
+* Youtube: https://www.youtube.com/channel/UCliHyp1bvd0MbcRSkZcPhhw
+* Instagram: https://www.instagram.com/clan__fcla/
+* Facebook: https://www.facebook.com/ArmA3FCLA/
+
+> Estos mods fueron agrupados dentro de un mod pack para que sea más facil cargarlos en un commandline.
+Ademas algunos de estos han sido modificados en base a las necesidades de la comunidad (traducciones, funciones inesarias, etc.).
---
+++ mod.cpp
@@ -0,0 +1,17 @@
+
+/********************************************************************************|
+|                              PRESENTACIÓN DEL MOD                              |
+|********************************************************************************/
+
+name = "FCLA Mod Pack";
+logo = "logo.paa";
+author = "hozlucas28";
+action = "https://steamcommunity.com/sharedfiles/filedetails/?id=2740928463";
+picture = "picture.paa";
+overview = "¡FCLA Mod Pack trata de un conjunto de mods para la comunidad de Arma 3 FCLA! <br/>Fueron agrupados dentro de un mod pack para que sea más facil cargarlos en un commandline. Algunos de estos han sido modificados en base a las necesidades de la comunidad (traducciones, funciones inesarias, etc.).";
+logoOver = "logoOver.paa";
+logoSmall = "logoSmall.paa";
+actionName = "MODS";
+tooltipOwned = "¡FCLA Mod Pack trata de un conjunto de mods para la comunidad de Arma 3 FCLA! <br/>Fueron agrupados dentro de un mod pack para que sea más facil cargarlos en un commandline. Algunos de estos han sido modificados en base a las necesidades de la comunidad (traducciones, funciones inesarias, etc.).";
+hideName = 0;
+hidePicture = 0;
